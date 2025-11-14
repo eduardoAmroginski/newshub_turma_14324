148c92f86376dad870a3203ac76eb653792c53e6
VERIDIANA CIDAL MENDES <73212300078@senacrs.edu.br>
2025-11-13 21:52:37 -0300

    formatação  do Card

--- src/assets/componentes/cardNoticias/CardNoticias.js
+++ src/assets/componentes/cardNoticias/CardNoticias.js
@@ -2,10 +2,10 @@ class CardNoticias extends HTMLElement {
     set data(noticia) {
         console.log("noticia: ", noticia)
         this.innerHTML=`
-        <div class= 'card'>
+        <div class= 'card card-noticias'>
             <div class= 'card-body text-center'>
                 <h4 class='card-categoria'>Categoria</h4>
-                <p class='card-text'>cardcategoria</p>
+                <p class='card-text text-noticia'>cardcategoria</p>
                 <h5 class='card-descricao-noticia'>DecriçãoNoticia</h5>
             </div>
         </div>`;
