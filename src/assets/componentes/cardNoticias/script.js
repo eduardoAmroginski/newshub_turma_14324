1c0e55f41568d588fdbbdf1658cbfa1eefdf9c55
VERIDIANA CIDAL MENDES <73212300078@senacrs.edu.br>
2025-11-21 20:43:35 -0300

    Adicionando web componente de card-noticias

---
+++ src/assets/componentes/cardNoticias/script.js
@@ -0,0 +1,12 @@
+import './CardNoticias.js'
+
+const containerCard = document.getElementById("container-card")
+const card1 = document.createElement("card-noticias")
+card1.data = "TESTE";
+
+containerCard.appendChild(card1);
+
+const card2 = document.createElement("card-noticias")
+card2.data = "TESTE";
+
+containerCard.appendChild(card2);
