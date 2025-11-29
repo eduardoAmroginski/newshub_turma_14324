9d18f94a6728236a833b2397d8f050ea68389684
Dionatan Cardoso <dionatancardoso22@hotmail.com>
2025-11-28 21:43:13 -0300

    Inserido carrossel e alterado regras css

--- src/assets/js/customScript.js
+++ src/assets/js/customScript.js
@@ -1,4 +1,11 @@
-const activeCategory = document.getElementsByTagName("button");
+import {fetchTopHeadlines} from "./service/api.js";
 
+const headlinesArticle = fetchTopHeadlines();
 
+const headlinesTitles = headlinesArticles.title();
 
+for (let i;i<6;i++){
+    document.getElementsByClassName("titulo", funtion(){
+        document.appendChild(headlinesTitles);
+    });
+}
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,2 +1,3 @@
 import "../../assets/componentes/footer/insertFooter.js"
 import "../../assets/componentes/lista-noticias-alta/insertListaNoticiaAlta.js"
+import "../../assets/componentes/carrossel-categoria/insert-carrossel-cat.js"
